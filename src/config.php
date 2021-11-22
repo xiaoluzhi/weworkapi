1189b1619bb1521d2c654c3f3462d77c166448b6
Xiaolu <32961691+xiaoluzhi@users.noreply.github.com>
2021-11-22 15:51:06 +0800

    Update config.php

--- src/config.php
+++ src/config.php
@@ -11,5 +11,5 @@
 
 // 配置文件。corpid、secret等配置信息，请参考 examples/config.php。实际使用其正式配置带进来。
 return array(
-    'DEBUG' => true, // 是否打印debug信息。如果这里为 true，则返回的数据中包括 post 请求相关信息，而不仅仅是json格式输出。
+    'DEBUG' => false, // 是否打印debug信息。如果这里为 true，则返回的数据中包括 post 请求相关信息，而不仅仅是json格式输出。
 );
